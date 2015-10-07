4a9977b0a9ff14f1645c933fe2eaa40c69e3d40a
Alejandro Castro <alejo@securelink.com>
2015-10-07 10:56:37 -0500

    Showing a message if google maps is not available

--- js/map.js
+++ js/map.js
@@ -1,27 +1,33 @@
 var $ = require("jquery");
 
-/* Map initialization properties */
-var mapOptions = {
-  center: new google.maps.LatLng(-34.6079958, -58.3766988),
-  disableDoubleClickZoom: true,
-  keyboardShortcuts: false,
-  mapTypeControl: false,
-  mapTypeId: google.maps.MapTypeId.ROADMAP,
-  panControl: false,
-  scaleControl: true,
-  streetViewControl: false,
-  styles: [{
-      featureType: 'all',
-      elementType: 'labels.icon',
-      stylers: [{ visibility: 'off' }]
-    }],
-  zoom: 14
-};
-
 /**
  * This is the map that we will display on the page
  * @constructor
  */
 module.exports = function() {
-	return new google.maps.Map($('#js-map-canvas')[0], mapOptions);
+  if (typeof(google) == 'undefined') {
+    alert('Unable to load the map at this time. Please try later.');
+    return {};
+  }
+
+  try {
+    return new google.maps.Map($('#js-map-canvas')[0], {
+      center: new google.maps.LatLng(-34.6079958, -58.3766988),
+      disableDoubleClickZoom: true,
+      keyboardShortcuts: false,
+      mapTypeControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      panControl: false,
+      scaleControl: true,
+      streetViewControl: false,
+      styles: [{
+          featureType: 'all',
+          elementType: 'labels.icon',
+          stylers: [{ visibility: 'off' }]
+        }],
+      zoom: 14
+    });
+  } catch (e) {
+    alert('Unable to load the map at this time. Please try later.');
+  }
 };
